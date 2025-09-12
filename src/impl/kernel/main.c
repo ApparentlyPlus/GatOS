ae20c4c11f1b6614d7c56192fa0a96163f8ac647
ApparentlyPlus <panoschatzikallias@gmail.com>
2025-09-12 20:01:33 +0300

    Update kernel version to v1.5.5 and enhance logging

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -17,7 +17,7 @@
 
 #define TOTAL_DBG 7
 
-static char* KERNEL_VERSION = "v1.5.0";
+static char* KERNEL_VERSION = "v1.5.5";
 static uint8_t multiboot_buffer[8 * 1024]; // 8KB should be more than enough
 
 /*
@@ -43,20 +43,24 @@ void kernel_main(void* mb_info) {
 
 	// Extend the kernel region to include space for the page tables to map all physical memory
 	reserve_required_tablespace(&multiboot);
+	print("[MEM] Kernel region extended to include page tables.\n");
 	DEBUG("Reserved the required space for page tables in the kernel region", TOTAL_DBG);
 
 	// Unmap anything besides [0, KPHYS_END] and [HH_BASE, HH_BASE + KPHYS_END]
 	cleanup_kernel_page_tables(0x0, get_kend(false));
+	print("[MEM] Cleaned up page tables, unmapped everything besides the kernel range.\n");
 	DEBUG("Unmapped all memory besides the kernel range", TOTAL_DBG);
 	
 	// Unmap [0, KPHYS_END], we only have [HH_BASE, HH_BASE + KPHYS_END] mapped
 	unmap_identity();
+	print("[MEM] Unmapped identity mapping, only higher half remains.\n");
 	DEBUG("Unmapped identity mapping, only higher half remains", TOTAL_DBG);
 
 	// Build the physmap (mapping of all physical RAM into virtual space)
 	build_physmap();
+	print("[MEM] Built physmap, all physical memory is now accessible.\n");
 	DEBUG("Built physmap at PHYSMAP_VIRTUAL_BASE", TOTAL_DBG);
-	
+
 	// Final sanity check
 	check_kernel_position();
 	DEBUG("Reached kernel end", TOTAL_DBG);
