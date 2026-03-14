1f89bb18ae493f8ef7ebc42d5c78324caf621837
ApparentlyPlus <panoschatzikallias@gmail.com>
2026-03-14 15:21:09 +0200

    Bump kernel version to 1.9.9-alpha

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -41,7 +41,7 @@ extern void uapps(void);
 
 #define TOTAL_DBG 25
 
-static char* KERNEL_VERSION = "v1.9.8-alpha";
+static char* KERNEL_VERSION = "v1.9.9-alpha";
 
 // If it is a test build, the multiboot buffer will be defined in tests.c
 #ifndef TEST_BUILD
