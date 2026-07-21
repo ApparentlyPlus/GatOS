bb15eaa242e14e87cedf6ce36dbb9030d8b429f1
ApparentlyPlus <panoschatzikallias@gmail.com>
2026-07-22 01:37:15 +0300

    Bump kernel version to v2.1.8

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -18,7 +18,7 @@
 // Forward declaration of userspace app launcher
 extern void uapps(void);
 
-static char* KERNEL_VERSION = "v2.1.7";
+static char* KERNEL_VERSION = "v2.1.8";
 
 /*
  * kernel_main - Main entry point for the GatOS kernel
